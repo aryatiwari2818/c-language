586d94503337b9972152669ad1e40db608768abe
aryatiwari2818 <aryatiwari2818@gmail.com>
2025-09-04 14:08:00 +0530

    add folder lect6

---
+++ lect7/index7.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+int main()
+{
+    /*int num, isprime=1;
+    printf("enter num=");
+    scanf("%d",&num);
+    int i;
+    if(num<=1)
+    printf("not prime");
+    return 0;
+    for(i=2;i<num;i++)
+    {
+        if(num%i==0)
+        {
+            isprime=0;
+            break;
+        }
+    }
+    if(isprime)
+    {
+        printf("%d is prime",num);
+    }
+    else
+    {
+        printf("not prime");
+    }
+    }*/
+    /*int t1=0;
+    int t2=1;
+    int next;
+    printf("fibonacci series");
+    for(int i=1;i<=10;i++)
+    {
+        printf("%d\n",t1);
+        next=t1+t2;
+        t1=t2;
+        t2=next;
+    }
+    return 0;}
+    */
+    /*int i;
+    for (i = 0; i < 5; i++)
+    {
+        {
+            if (i == 3)
+               // break;
+               continue;
+            }
+        printf("%d", i);
+    }
+    return 0;
+}*/
+
+/* int t1 = 0;
+ int t2 = 1;
+ int next;
+ int i = 1, n;
+ printf("enter num");
+ scanf("%d", &n);
+
+     while (i < n)
+ {
+     printf("%d\n", t1);
+     next = t1 + t2;
+     t1 = t2;
+     t2 = next;
+     i++;
+ }
+ return 0;
+}*/
+/*int count = 0;
+int n;
+printf("enter num=");
+scanf("%d", &n);
+if (n ==0)
+{
+    count = 0;
+}
+while (n > 0)
+{
+    count++;
+    n = n / 10;
+}
+printf("num ofdigit=%d", count);
+return 0;
+}*/
