c73e585ffc407ea3a4e68f081ff11eaa61d5f12e
aryatiwari2818 <aryatiwari2818@gmail.com>
2025-10-06 22:43:40 +0530

    add lect19 & lect20

---
+++ lect20/index20.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    //      char name[]="MADAM";
+    //  int ln=strlen(name);
+    // int istrue=1;
+    // for(int i=0;i<ln/2;i++){
+    //     if(name[i]!=name[ln-i-1]){
+    //         istrue=0;
+    // printf(" not a palindrome");
+    //         break;
+    //     }}
+    // if(istrue=1){
+    //     printf("PALINDROME");
+    // }
+
+    // // }
+    // char name[] = "madam";
+    // char data[10];
+    // for (int i = 0; name[i] != '\0'; i++)
+    // {
+    //     printf("%c", name[i]);
+    //     data[i] = name[i];
+    // }
+    // printf("%s\n", data);
+    // int ln = strlen(name);
+    // printf("%d\n", ln);
+    
+    // int i=0;
+    // while (name[i] != '\0')
+    // {
+    //     printf("%c\n", name[i]);
+    //         i++;
+    // }
+    // return 0;
+// }
+// 
+//achar name[50];
+// printf("enter your name=");
+// scanf("%s",name);
+// printf("you have entered:%s",name);
+// return 0;}
+char name[50];
+ printf("enter your name=");
+gets(name);
+
+ printf("you have entered:%s\n",name);
+ return 0;}
